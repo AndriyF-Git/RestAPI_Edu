f0e959bc5e9f4942374657771a011e17bb85e98c
andriy <fedirko816@gmail.com>
2025-09-02 11:28:25 +0300

    upload and download files

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-import time
-import asyncio
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse, StreamingResponse
 
 app = FastAPI()
 
-def sync_task():
-    time.sleep(3)
-    print("sync task")
+@app.post('/files')
+async def upload_file(uploaded_file: UploadFile):
+    file =  uploaded_file.file
+    filename = uploaded_file.filename
+    with (open(f"1_{filename}", 'wb') as f):
+        f.write(file.read())
 
-async def async_task():
-    await asyncio.sleep(3)
-    print("async task")
 
-@app.post('/')
-async def some_route(bg_tasks: BackgroundTasks):
-    ...
-    # asyncio.create_task(async_task())
-    bg_tasks.add_task(sync_task)
-    return {'ok': True}
+@app.post('/multiple_files')
+async def upload_files(uploaded_files: list[UploadFile]):
+    for uploaded_file in uploaded_files:
+        file =  uploaded_file.file
+        filename = uploaded_file.filename
+        with open(f"1_{filename}", 'wb') as f:
+            f.write(file.read())
+
+
+@app.get('/files/{filename}')
+async def get_file(filename: str):
+    return FileResponse(filename)
+
+def iterfile(filename: str):
+    with open(filename, 'rb') as file:
+        while chunk := file.read(1024 * 1024):
+            yield chunk
+
+
+@app.get('/files/streaming{filename}')
+async def get_streaming_file(filename: str):
+    return StreamingResponse(iterfile(filename), media_type='text/txt')
