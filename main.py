9c4e4692b18c9c9ba7066538e80a573b9c3b39a0
andriy <fedirko816@gmail.com>
2025-09-03 15:06:17 +0300

    Docker build

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-books = [
-    {
-        "id": 1,
-        "title": "Python Programming",
-        "author": "1", },
-    {
-        "id": 2,
-        "title": "Python Programming1",
-        "author": "2", },
-]
+@app.get("/users")
+async def get_users():
+    return {"users": [{"id": 1, "name": "And"}, {"id": 2, "name": "DNA"}]}
 
-class BookSchema(BaseModel):
-    title: str
-    author: str
-
-@app.get("/books",
-         tags=["books"],
-         summary='get all books',
-         description="<h1>Get all books</h1>")
-def get_books():
-    return books
-
-@app.post("/books", tags=["books"])
-def add_book(book: BookSchema):
-    new_book_id = len(books) + 1
-    books.append({
-        "id": new_book_id,
-        "title": book.title,
-        "author": book.author,
-    })
-    return {"success": True, "message": "book added"}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
